4ac7de14c3f3934f72cf6f4b2f84ab8d7aa622bc
Charlie Rogers <charlie.rogers@cohesionforce.com>
2024-04-22 15:23:27 -0500

    defining pipeline setup

--- holodeck/phb_rag/pipeline.py
+++ holodeck/phb_rag/pipeline.py
@@ -1,6 +1,13 @@
-from .client import get_embeddings_client, get_generative_client
+from .client import get_embeddings_client, get_generative_client, setup_embed, setup_generate   
+import holodeck.utilities.constants as constants
+from loguru import logger
 
-gen_client = client.get_generative_client(host=constants.OLLAMA_LOCAL_URL)
-embed_client = client.get_embeddings_client(host=constants.OLLAMA_LOCAL_URL)
-
+def pipeline_setup():
     
+    logger.info("Setting up generative and embedding clients...")
+    gen_client = get_generative_client(host=constants.OLLAMA_LOCAL_URL)
+    embed_client = get_embeddings_client(host=constants.OLLAMA_LOCAL_URL)
+
+    setup_embed(embed_client, constants.DEFAULT_EMBEDDING_MODEL)
+    setup_generate(gen_client, constants.DEFAULT_GENERATOR_MODEL)
+
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import holodeck.phb_rag.pipeline as pipeline
-import holodeck.phb_rag.client as client
 import holodeck.utilities.constants as constants
 import holodeck.utilities.custom_logging as custom_logging
 
 def main():
     
     custom_logging.setup_logger()
+    pipeline.pipeline_setup()
     
-    client.setup_embed(embed_client, constants.DEFAULT_EMBEDDING_MODEL)
-    client.setup_generate(gen_client, constants.DEFAULT_GENERATOR_MODEL)
+    
     
     
     
